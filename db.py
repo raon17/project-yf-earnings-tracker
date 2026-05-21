3697ad9b2738e176cbc835042acd696eb16f9c36
William Kim <willkim0117@gmail.com>
2026-05-21 21:24:17 +1000

    Debugging - ppdated db connection to static python dictionary

--- db.py
+++ db.py
@@ -6,11 +6,11 @@ from dotenv import load_dotenv
 load_dotenv()
 
 DB_CONFIG = {
-    "host":     os.getenv("DB_HOST", "localhost"),
-    "port":     os.getenv("DB_PORT", 5432),
-    "dbname":   os.getenv("DB_NAME", "earnings_tracker"),
-    "user":     os.getenv("DB_USER", "postgres"),
-    "password": os.getenv("DB_PASSWORD", ""),
+    "host": os.getenv("DB_HOST", "localhost"),
+    "port": os.getenv("DB_PORT", 5432),
+    "dbname": os.getenv("DB_NAME", "earnings_tracker"),
+    "user": os.getenv("DB_USER", "postgres"),
+    "password":os.getenv("DB_PASSWORD", ""),
 }
 
 def get_conn():
